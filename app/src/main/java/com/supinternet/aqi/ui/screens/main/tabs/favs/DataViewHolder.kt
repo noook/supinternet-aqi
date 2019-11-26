ec8b7b76959bb3971722a3c5d93dd3324f1142f7
root <root@DESKTOP-E83FCSA.localdomain>
2019-11-26 21:53:10 +0100

    Fill recycler view with datas

---
+++ app/src/main/java/com/supinternet/aqi/ui/screens/main/tabs/favs/DataViewHolder.kt
@@ -0,0 +1,41 @@
+package com.supinternet.aqi.ui.screens.main.tabs.favs
+
+import android.text.format.DateUtils
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import com.supinternet.aqi.R
+import com.supinternet.aqi.data.network.model.station.Data
+import java.text.SimpleDateFormat
+import java.util.*
+
+class DataViewHolder(inflater: LayoutInflater, parent: ViewGroup) :
+    RecyclerView.ViewHolder(inflater.inflate(R.layout.fragment_maps_station_card, parent, false)) {
+    private var name: TextView? = null
+    private var aqi: TextView? = null
+    private var date: TextView? = null
+    private var cardView: CardView? = null
+
+
+    init {
+        cardView = itemView.findViewById(R.id.cardView_station)
+        name = itemView.findViewById(R.id.maps_tab_station_name)
+        aqi = itemView.findViewById(R.id.maps_tab_station_aqi_value)
+        date = itemView.findViewById(R.id.maps_tab_station_date_value)
+    }
+
+    fun bind(data: Data) {
+        name?.text = data.city.name
+        aqi?.text = data.aqi.toString()
+        date?.text = DateUtils.getRelativeTimeSpanString(
+            SimpleDateFormat(
+                "yyyy-MM-dd HH:mm:ss",
+                Locale.ENGLISH
+            ).parse(data.time.s).time).toString()
+        cardView?.visibility = View.VISIBLE
+    }
+
+}
--- app/src/main/java/com/supinternet/aqi/ui/screens/main/tabs/favs/FavsTab.kt
+++ app/src/main/java/com/supinternet/aqi/ui/screens/main/tabs/favs/FavsTab.kt
@@ -3,12 +3,17 @@ package com.supinternet.aqi.ui.screens.main.tabs.favs
 import android.app.AlertDialog
 import android.content.Context
 import android.os.Bundle
+import android.text.format.DateUtils
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -20,11 +25,16 @@ import com.supinternet.aqi.data.network.AQIAPI
 import com.supinternet.aqi.data.network.model.map.MapSearch
 import com.supinternet.aqi.data.network.model.search.TextSearch
 import com.supinternet.aqi.data.network.model.station.Data
+import kotlinx.android.synthetic.main.fragment_favs.*
+import kotlinx.android.synthetic.main.fragment_maps.*
+import kotlinx.android.synthetic.main.fragment_maps_station_card.*
 import kotlinx.coroutines.*
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
+import java.text.SimpleDateFormat
+import java.util.*
 
 data class Fav(val stationName : String,  val stationId: Int)
 
@@ -46,7 +56,6 @@ class FavsTab : Fragment() {
         val table = mutableListOf<Data>()
         super.onViewCreated(view, savedInstanceState)
 
-        // Fausse données
         var data = arrayOf(Fav("Paris",5722), Fav("Lyon",3028), Fav("Toulouse",3836))
 
         GlobalScope.launch(Dispatchers.Default) {
@@ -61,24 +70,17 @@ class FavsTab : Fragment() {
                     ).await()
 
                 table.add(res.data)
-                Log.v("HELLO",res.data.toString())
             }
 
 
             withContext(Dispatchers.Main) {
-                //textview.text := res.data )
-
+                Log.v("HELLO",table.toString())
+                lists.apply {
+                    layoutManager = LinearLayoutManager(activity)
+                    adapter = ListAdapter(table)
+                    determinateBar?.visibility = View.GONE
+                }
             }
-
         }
-        Log.v("abcd",table.toString())
-
-
-
-        //Log.v("abcde",res.data.toString())
-
-
     }
-
-
 }
