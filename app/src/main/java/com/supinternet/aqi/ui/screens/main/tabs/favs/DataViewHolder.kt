ba9c620241b95f05553cbeb2e8bcd47f48c0c557
Pakopac <lilian.pacaud@supinternet>
2019-11-27 14:55:34 +0100

    Add loader and change color aqi

--- app/src/main/java/com/supinternet/aqi/ui/screens/main/tabs/favs/DataViewHolder.kt
+++ app/src/main/java/com/supinternet/aqi/ui/screens/main/tabs/favs/DataViewHolder.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.supinternet.aqi.R
 import com.supinternet.aqi.data.network.model.station.Data
@@ -30,6 +31,18 @@ class DataViewHolder(inflater: LayoutInflater, parent: ViewGroup) :
     fun bind(data: Data) {
         name?.text = data.city.name
         aqi?.text = data.aqi.toString()
+        aqi?.setTextColor(
+        ContextCompat.getColor(
+            name!!.context, when (data.aqi) {
+                in 0..50 -> R.color.aqi_good
+                in 51..100 -> R.color.aqi_moderate
+                in 101..150 -> R.color.aqi_unhealthy_sensitive
+                in 151..200 -> R.color.aqi_unhealthy
+                in 201..300 -> R.color.aqi_very_unhealthy
+                else -> R.color.aqi_hazardous
+            }
+        )
+        )
         date?.text = DateUtils.getRelativeTimeSpanString(
             SimpleDateFormat(
                 "yyyy-MM-dd HH:mm:ss",
