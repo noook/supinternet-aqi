ac8b44bba98462935466bc116df7507ac0e092cd
noook <me@neilrichter.com>
2019-11-27 12:13:10 +0100

    ✨ Added dynamic image and string loading + api call

--- app/src/main/java/com/supinternet/aqi/ui/screens/main/tabs/travel/CityRecyclerView.kt
+++ app/src/main/java/com/supinternet/aqi/ui/screens/main/tabs/travel/CityRecyclerView.kt
@@ -1,5 +1,11 @@
 package com.supinternet.aqi.ui.screens.main.tabs.travel
 
+import android.content.Context
+import android.graphics.Typeface
+import android.provider.Settings.Global.getString
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.StyleSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -7,9 +13,11 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.supinternet.aqi.R
+import com.supinternet.aqi.data.network.model.ranking.City
 import kotlinx.android.synthetic.main.travel_tab_item_cell.view.*
+import java.lang.Exception
 
-class CityAdapter(val cities: List<CityPollution>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class CityAdapter(val cities: List<City>, val context: Context) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         return CityItemCell(
             LayoutInflater.from(parent.context)
@@ -22,19 +30,50 @@ class CityAdapter(val cities: List<CityPollution>) : RecyclerView.Adapter<Recycl
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        (holder as CityItemCell).bindCity(cities[position])
+
+        (holder as CityItemCell).bindCity(cities[position], position, context)
     }
 }
 
 class CityItemCell(v: View) : RecyclerView.ViewHolder(v) {
     //private val flag: ImageView = v.country_flag
+    private val cityPosition: TextView = v.city_position
     private val name: TextView = v.city_name
     private val flag: ImageView = v.city_flag
     private val pollution: TextView = v.pollution_value
 
-    fun bindCity(city: CityPollution) {
-        name.text = city.name
-        flag.setImageResource(R.drawable.ic_flag_country_in)
-        pollution.text = city.pollution.toString()
+    fun bindCity(city: City, position: Int, context: Context) {
+        val country = try {
+            context.getString(
+                context.resources.getIdentifier(
+                    "country_${city.country}_name",
+                    "string",
+                    context.packageName
+                )
+            )
+        } catch (e: Exception) {
+            city.country
+        }
+        val flagRes: Int = try {
+            context.resources.getIdentifier(
+                "ic_flag_country_${city.country.toLowerCase()}",
+                "drawable",
+                context.packageName
+            )
+        } catch (e: Exception) {
+            R.drawable.ic_flag_country_fr
+        }
+
+        name.setCityAndCountry(city.city, country)
+        cityPosition.text = (position + 1).toString()
+
+        flag.setImageResource(flagRes)
+        pollution.text = city.station.a
+    }
+}
+
+fun TextView.setCityAndCountry(city: String, country: String) {
+    text = SpannableString("$city: $country").apply {
+        setSpan(StyleSpan(Typeface.BOLD), 0, city.length + 1, Spannable.SPAN_INCLUSIVE_INCLUSIVE)
     }
 }
--- app/src/main/java/com/supinternet/aqi/ui/screens/main/tabs/travel/TravelTab.kt
+++ app/src/main/java/com/supinternet/aqi/ui/screens/main/tabs/travel/TravelTab.kt
@@ -1,13 +1,20 @@
 package com.supinternet.aqi.ui.screens.main.tabs.travel
 
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.supinternet.aqi.R
+import com.supinternet.aqi.data.network.RankingAPI
 import kotlinx.android.synthetic.main.travel_tab_recycler_view.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class TravelTab : Fragment() {
     override fun onCreateView(
@@ -21,15 +28,28 @@ class TravelTab : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val cities = listOf<CityPollution>(
-            CityPollution(name = "Paris", pollution = 54),
-            CityPollution(name = "Brest", pollution = 10),
-            CityPollution(name = "Amsterdam", pollution = 22),
-            CityPollution(name = "New York", pollution = 112),
-            CityPollution(name = "Berlin", pollution = 12)
-        )
+        GlobalScope.launch(Dispatchers.Main) {
+            try {
+                val rankings = withContext(Dispatchers.IO) {
+                    RankingAPI.getInstance().getRanking().await()
+                }
+                val sorted = rankings.cities.sortedByDescending {
+                    it.station.a.toInt()
+                }
 
-        travel_tab_recycler_view.layoutManager = LinearLayoutManager(requireContext())
-        travel_tab_recycler_view.adapter = CityAdapter(cities)
+                /* val id = Settings.Global.getString(
+                    resources.getIdentifier(
+                        "country_${city.country}_name",
+                        "string",
+                        requireContext().packageName
+                    )
+                )*/
+
+                travel_tab_recycler_view.layoutManager = LinearLayoutManager(requireContext())
+                travel_tab_recycler_view.adapter = CityAdapter(sorted, requireContext())
+            } catch (e: Exception) {
+                print(e)
+            }
+        }
     }
 }
