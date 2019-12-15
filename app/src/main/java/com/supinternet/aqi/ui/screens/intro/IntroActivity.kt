9018dbb56ae6362bc2db79f431beb1e24930f036
noook <me@neilrichter.com>
2019-12-15 20:08:13 +0100

    ✨ Added google sign-in

--- app/src/main/java/com/supinternet/aqi/ui/screens/intro/IntroActivity.kt
+++ app/src/main/java/com/supinternet/aqi/ui/screens/intro/IntroActivity.kt
@@ -4,6 +4,12 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.auth.api.signin.GoogleSignInResult
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
 import com.supinternet.aqi.R
 import com.supinternet.aqi.ui.screens.main.MainActivityIntent
 import kotlinx.android.synthetic.main.activity_intro.*
@@ -13,15 +19,49 @@ fun Context.IntroActivityIntent(): Intent {
     return Intent(this, IntroActivity::class.java)
 }
 
-class IntroActivity: AppCompatActivity() {
+class IntroActivity : AppCompatActivity(), GoogleApiClient.OnConnectionFailedListener {
+    private val RC_SIGN_IN = 9001
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
 
-        intro_login_button.setOnClickListener {
-            // TODO Connexion Google
+        val googleSignInOptions: GoogleSignInOptions = GoogleSignInOptions
+            .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+
+        val googleApiClient = GoogleApiClient
+            .Builder(this)
+            .enableAutoManage(this, this)
+            .addApi(Auth.GOOGLE_SIGN_IN_API, googleSignInOptions)
+            .build()
+
+        sign_in_button.setOnClickListener {
+            val intent: Intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient)
+            startActivityForResult(intent, RC_SIGN_IN)
+        }
+    }
+
+    override fun onConnectionFailed(p0: ConnectionResult) {
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val result: GoogleSignInResult = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+            handleSignInResult(result)
+        }
+    }
+
+    private fun handleSignInResult(signInResult: GoogleSignInResult) {
+        if (signInResult.isSuccess) {
+            val account: GoogleSignInAccount? = signInResult.signInAccount
             startActivity(MainActivityIntent())
+        } else {
+            // Failed
         }
     }
 
--- app/src/main/java/com/supinternet/aqi/ui/screens/splashscreen/SplashScreenActivity.kt
+++ app/src/main/java/com/supinternet/aqi/ui/screens/splashscreen/SplashScreenActivity.kt
@@ -1,16 +1,34 @@
 package com.supinternet.aqi.ui.screens.splashscreen
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
 import com.supinternet.aqi.ui.screens.intro.IntroActivityIntent
 import com.supinternet.aqi.ui.screens.main.MainActivityIntent
 
-class SplashScreenActivity : AppCompatActivity() {
+class SplashScreenActivity : AppCompatActivity(), GoogleApiClient.OnConnectionFailedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        if (isUserConnected()) {
+        val googleSignInOptions: GoogleSignInOptions = GoogleSignInOptions
+            .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+
+        val googleApiClient = GoogleApiClient
+            .Builder(this)
+            .enableAutoManage(this, this)
+            .addApi(Auth.GOOGLE_SIGN_IN_API, googleSignInOptions)
+            .build()
+
+        if (isUserConnected(googleApiClient)) {
             startActivity(MainActivityIntent())
         } else {
             startActivity(IntroActivityIntent())
@@ -19,8 +37,21 @@ class SplashScreenActivity : AppCompatActivity() {
         finish()
     }
 
-    private fun isUserConnected() : Boolean {
-        // TODO Rechercher si l'utilisateur est déjà connecté
+    private fun isUserConnected(client: GoogleApiClient): Boolean {
+        val googleSignInAccount = GoogleSignIn.getLastSignedInAccount(this)
+        if (googleSignInAccount != null) {
+            return true
+        }
         return false
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        Log.d("REQ_CODE", requestCode.toString())
+    }
+
+    override fun onConnectionFailed(p0: ConnectionResult) {
+
     }
 }
